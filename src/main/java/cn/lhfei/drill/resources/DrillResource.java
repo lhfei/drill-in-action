16f68848f60319f916689dd78170db05206affdf
Hefei Li <lhfeilaile@gmail.com>
2018-10-11 10:59:54 +0800

    refactor: get data by jdbc

--- src/main/java/cn/lhfei/drill/resources/DrillResource.java
+++ src/main/java/cn/lhfei/drill/resources/DrillResource.java
@@ -18,17 +18,22 @@ package cn.lhfei.drill.resources;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 /**
  * @version 0.1
@@ -42,25 +47,52 @@ import org.springframework.web.bind.annotation.RestController;
 public class DrillResource extends AbstractResource {
 	
 	@RequestMapping(value = "/files", method = GET)
-	public List<String> getFiles(@RequestParam Integer limit) throws ClassNotFoundException, SQLException {
+	public ArrayNode getFiles(@RequestParam Integer limit) throws ClassNotFoundException, SQLException {
 		LOG.debug(MARKER, "getFiles method execute ...");
 		//String sql = "SELECT * FROM dfs.`/user/druid/benchmark/data/lineitem.tbl` LIMIT ?";
 		String sql = "SELECT * FROM hive.benchmark.lineitem LIMIT ?";
 		
 		List<String> result = new ArrayList<>();
 		
-//		Class.forName("org.apache.drill.jdbc.Driver");
-//		Connection connection = DriverManager.getConnection("jdbc:drill:zk=host-10-182-60-8:2181,host-10-182-60-113:2181,host-10-182-60-142:2181,host-10-182-60-149:2181/drill/drillbits-li");
+		Class.forName("org.apache.drill.jdbc.Driver");
+		Connection connection = DriverManager.getConnection("jdbc:drill:zk=host-10-182-60-8:2181,host-10-182-60-113:2181,host-10-182-60-142:2181,host-10-182-60-149:2181/drill/drillbits-li");
 		
-		/*Connection connection = dataSource.getConnection();
-		Statement st = connection.createStatement();
+		PreparedStatement ps = connection.prepareStatement(sql);
+		ps.setInt(1, limit);
 		
-		ResultSet rs = st.executeQuery("SELECT * FROM dfs.`/user/druid/benchmark/data/lineitem.tbl` LIMIT 20");
+		ResultSet rs = ps.executeQuery(sql);
+		
+		ObjectMapper mapper = new ObjectMapper();
+
+		ArrayNode items = mapper.createArrayNode();
+		
+		
 		while(rs.next()){
 			LOG.debug(MARKER, rs.getString(1));
-		}*/
+			
+			ObjectNode item = mapper.createObjectNode();
+			
+			item.put("l_orderkey"     , rs.getString("l_orderkey"));     
+			item.put("l_partkey"      , rs.getString("l_partkey"));      
+			item.put("l_suppkey"      , rs.getString("l_suppkey"));      
+			item.put("l_linenumber"   , rs.getString("l_linenumber"));   
+			item.put("l_quantity"     , rs.getString("l_quantity"));     
+			item.put("l_extendedprice", rs.getString("l_extendedprice"));
+			item.put("l_discount"     , rs.getString("l_discount"));     
+			item.put("l_tax"          , rs.getString("l_tax"));          
+			item.put("l_returnflag"   , rs.getString("l_returnflag"));   
+			item.put("l_linestatus"   , rs.getString("l_linestatus"));   
+			item.put("l_shipdate"     , rs.getString("l_shipdate"));     
+			item.put("l_commitdate"   , rs.getString("l_commitdate"));   
+			item.put("l_receiptdate"  , rs.getString("l_receiptdate"));  
+			item.put("l_shipinstruct" , rs.getString("l_shipinstruct")); 
+			item.put("l_shipmode"     , rs.getString("l_shipmode"));     
+			item.put("l_comment"      , rs.getString("l_comment"));      
+			
+			items.add(item);
+		}
 		
-		jdbcTemplate.query(sql, new Object[] {limit}, new RowMapper<List<String>>() {
+		/*jdbcTemplate.query(sql, new Object[] {limit}, new RowMapper<List<String>>() {
 
 			@Override
 			public List<String> mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -69,11 +101,11 @@ public class DrillResource extends AbstractResource {
 				}
 				return result;
 			}
-		});
+		});*/
 		
-		return result;
+		return items;
 	}
 	
-	@Autowired
+//	@Autowired
 	private JdbcTemplate jdbcTemplate;
 }
